bcec2a1f722af026a537a8a2e7b0cf1f6337703c
MichaelBabiy23 <mr.babiymichael@gmail.com>
2024-12-03 20:31:06 +0200

    -modified load_data function -added group_and_aggregate_data, remove_sparse_columns functions

--- main.py
+++ main.py
@@ -1,14 +1,47 @@
+import numpy as np
 import pandas as pd
 
 
 def load_data(filepath: str) -> pd.DataFrame:
-    '''
+    """
     loading data from excel files
-    :param filepath:
+    :param filepath: String
     :return: dataFrame
-    '''
-    return pd.read_excel(filepath)
+    """
+    # try to open and read Excel file
+    try:
+        return pd.read_excel(filepath)
+    except Exception as e:
+        print(f"Error loading file: {e}")
+        return pd.DataFrame()
 
 
-data = load_data(filepath='data/knesset_25.xlsx')
-print(data)
+def group_and_aggregate_data(df: pd.DataFrame, group_by_column: str, agg_func) -> pd.DataFrame:
+    """
+    grouping by and aggregate the data frame
+    :param df: DataFrame
+    :param group_by_column: String
+    :param agg_func: String array
+    :return:
+    """
+    # dropping ballot_code column from the df data frame, grouping by the group_by_column variable and applying agg_func aggregation
+    return df.drop(columns='ballot_code').groupby(group_by_column).agg(agg_func)
+
+
+def remove_sparse_columns(df: pd.DataFrame, threshold: int) -> pd.DataFrame:
+    """
+    remove sparse columns from dataframe
+    :param df: dataframe
+    :param threshold: integer
+    :return: dataframe
+    """
+    # identify numeric columns
+    numeric_columns = df.select_dtypes(include=[np.integer]).columns
+
+    # calculate the sum of each column
+    column_totals = df[numeric_columns].sum()
+
+    # return updated dataframe that suitable for the threshold
+    # column_totals[column_totals >= threshold] returns a series (label, index)
+    # the index information contains the labels of the rows
+    return df[column_totals[column_totals >= threshold].index]
